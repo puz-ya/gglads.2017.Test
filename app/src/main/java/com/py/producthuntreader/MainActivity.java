ab953a145da9e7f2d77c74f5c954eab3e1afb38a
puz_ya <puzino.y.a@gmail.com>
2017-03-08 01:46:36 +0300

    Added Category model, retrieving categories data and updating Drawer.

--- app/src/main/java/com/py/producthuntreader/MainActivity.java
+++ app/src/main/java/com/py/producthuntreader/MainActivity.java
@@ -1,8 +1,7 @@
 package com.py.producthuntreader;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -13,11 +12,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.BaseAdapter;
+import android.widget.HeaderViewListAdapter;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import com.py.producthuntreader.model.Category;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    public static final String LOG_TAG = "MainActivity: ";
     DrawerLayout mDrawer;
+    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,12 +67,14 @@ public class MainActivity extends AppCompatActivity
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mDrawer.openDrawer(Gravity.LEFT);
+                mDrawer.openDrawer(Gravity.START);
             }
         });
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        getSiteCategories();
     }
 
     @Override
@@ -80,6 +103,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            // will start new SettingsActivity for Service stop\start
             return true;
         } else if (id == R.id.action_close){
             finish();
@@ -94,23 +118,109 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        /*
+        if (id == R.id.nav_tech) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_books) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_games) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_podcast) {
 
         }
+        //*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
+    /** Get list of categories from site and insert into Drawer
+     * */
+    public void getSiteCategories(){
+        List<Category> listOfCategories = new ArrayList<>();
+
+        OkHttpClient client = new OkHttpClient();
+
+        String TOKEN = "";
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.producthunt.com/v1/categories").newBuilder();
+        urlBuilder.addQueryParameter("access_token", TOKEN);
+        String url = urlBuilder.build().toString();
+
+        // Add headers
+        Request request = new Request.Builder()
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "bearer " + TOKEN)
+                .addHeader("Host", "api.producthunt.com")
+                .url(url)
+                .build();
+
+        // Get a handler that can be used to post to the main thread
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                // Read data on the worker thread
+                final String responseData = response.body().string();
+                Log.d(LOG_TAG, responseData);
+
+                // Run view-related code back on the main thread
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        // Parsing JSON answer
+                        Gson gson = new GsonBuilder().create();
+
+                        JsonParser jsonParser = new JsonParser();
+                        JsonArray jsonCategories = jsonParser.parse(responseData)
+                                .getAsJsonObject().getAsJsonArray("categories");
+
+                        Category[] categories = gson.fromJson(jsonCategories, Category[].class);
+                        String[] categNames = new String[categories.length];
+                        int i = 0;
+                        for (Category category : categories) {
+                            categNames[i] = category.getName();
+                            Log.d(LOG_TAG, category.toString());
+                            i++;
+                        }
+
+                        updateDrawer(categNames);
+
+                    }
+                });
+            }
+        });
+
+    }
+
+    /** Updating Drawer elements and adapter
+     * @param titles - array of categories names
+     * */
+    public void updateDrawer(String[] titles){
+        Menu menu = mNavigationView.getMenu();
+        menu.clear();
+        for (String title : titles) {
+            menu.add(title);
+        }
+
+        for (int i = 0, count = mNavigationView.getChildCount(); i < count; i++) {
+            final View child = mNavigationView.getChildAt(i);
+            if (child != null && child instanceof ListView) {
+                final ListView menuView = (ListView) child;
+                final HeaderViewListAdapter adapter = (HeaderViewListAdapter) menuView.getAdapter();
+                final BaseAdapter wrapped = (BaseAdapter) adapter.getWrappedAdapter();
+                wrapped.notifyDataSetChanged();
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/py/producthuntreader/model/Category.java
@@ -0,0 +1,79 @@
+package com.py.producthuntreader.model;
+
+import java.util.Locale;
+
+/**
+ * Created by Puzino Yury on 07.03.2017.
+ */
+
+public class Category {
+
+    /** From API
+     * "id" : 1,
+     * "slug" : "tech",
+     * "name" : "Tech",
+     * "color" : "#da552f",
+     * "item_name" : "product"
+     * */
+
+    private Long id;
+    private String slug;
+    private String name;
+    private String color;
+    private String item_name;
+
+    public Category(Long id, String slug, String name, String color, String item_name){
+        this.id = id;
+        this.slug = slug;
+        this.name = name;
+        this.color = color;
+        this.item_name = item_name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.getDefault(),
+                "[Category: id=%1$d, slug=%2$s, name=%3$s, color=%4$s, item_name=%5$s]",
+                id, slug, name, color, item_name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+}
